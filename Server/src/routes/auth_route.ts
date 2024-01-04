160ba9db956da3b5973c8b949f749e4f4668f746
Amit Keinan <amit.keinan.9@gmail.com>
2024-01-04 18:36:35 +0200

    fix: Add login in register and fix refresh route

--- Server/src/routes/auth_route.ts
+++ Server/src/routes/auth_route.ts
@@ -1,6 +1,7 @@
 import express from "express";
-const router = express.Router();
 import authController from "../controllers/auth_controller";
+
+const router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
