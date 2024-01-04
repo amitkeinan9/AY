81f611e21aa122481c0f1295fe3b189ccc20a008
Amit Keinan <amit.keinan.9@gmail.com>
2024-01-04 18:38:26 +0200

    fix: small things like read headers and errors in server

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,27 +1,34 @@
 import env from "dotenv";
-env.config();
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import authRoute from "./routes/auth_route";
+import authMiddleware from "./common/auth_middleware";
 
-const initApp = (): Promise<Express> => {
-  const promise = new Promise<Express>((resolve) => {
-    
+env.config();
+
+const initApp = (): Promise<Express> =>
+  new Promise<Express>((resolve) => {
     const db = mongoose.connection;
+    const url = process.env.DB_URL;
+
     db.once("open", () => console.log("Connected to Database"));
     db.on("error", (error) => console.error(error));
-    const url = process.env.DB_URL;
-    
-    mongoose.connect(url!).then(() => {
+
+    if (!url) {
+      throw new Error("DB_URL is not defined");
+    }
+
+    mongoose.connect(url).then(() => {
       const app = express();
+
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true }));
+
       app.use("/auth", authRoute);
+
       resolve(app);
     });
   });
-  return promise;
-};
 
 export default initApp;
--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,19 +1,20 @@
 import initApp from "./app";
-import https from 'https';
-import http from 'http';
-import fs from 'fs';
+import https from "https";
+import http from "http";
+import fs from "fs";
 
 initApp().then((app) => {
-  if (process.env.NODE_ENV !== 'production') {
-    console.log('development', process.env.PORT);
+  if (process.env.NODE_ENV !== "production") {
+    console.log("development", process.env.PORT);
     http.createServer(app).listen(process.env.PORT);
+  } else {
+    // TODO: add pem for production mode
+    // const options = {
+    //   key: fs.readFileSync('../client-key.pem'),
+    //   cert: fs.readFileSync('../client-cert.pem')
+    // };
+
+    // https.createServer(options, app).listen(process.env.HTTPS_PORT);
+    https.createServer(app).listen(process.env.HTTPS_PORT);
   }
-  // TODO: add pem for production mode
-  // const options = {
-  //   key: fs.readFileSync('../client-key.pem'),
-  //   cert: fs.readFileSync('../client-cert.pem')
-  // };
-  
-  // https.createServer(options, app).listen(process.env.HTTPS_PORT);
-  https.createServer(app).listen(process.env.HTTPS_PORT);
 });
