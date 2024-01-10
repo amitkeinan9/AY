bb2a0474460a80b6ebb3c5c5a18c4496b1a3aa84
Amit Keinan <amit.keinan.9@gmail.com>
2024-01-10 21:18:40 +0200

    feat: Add posts page using react query

--- Client/src/pages/home/HomePage.tsx
+++ Client/src/pages/home/HomePage.tsx
@@ -1,3 +1,46 @@
+import { styled } from "@mui/system";
+import { Post } from "../../components/post/Post";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "react-query";
+import { backendAxiosInstance } from "../../axios/backendInstance";
+import { PostDTO } from "../../types/posts";
+import { CircularProgress } from "@mui/material";
+
+const PostsList = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  gap: 0,
+  width: "40vw",
+  borderRight: "1px solid rgba(239,243,244,1.00)",
+  borderLeft: "1px solid rgba(239,243,244,1.00)",
+});
+const Loader = styled(CircularProgress)({ margin: "30px auto" });
+
 export const HomePage = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  const { data, isLoading } = useQuery<PostDTO[]>(
+    "posts",
+    async () => (await backendAxiosInstance.get("/posts")).data
+  );
+
+  return (
+    <PostsList>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        data.map(({ _id, author, content }: PostDTO) => (
+          <Post
+            author={{
+              fullName: "",
+              username: author.email,
+              profilePic: "",
+            }}
+            content={content}
+            onClick={() => navigate(`/posts/${_id}`)}
+          />
+        ))
+      )}
+    </PostsList>
+  );
 };
