da1b85556052afed88033baaae7ad4c350b5c129
Amit Keinan <amit.keinan.9@gmail.com>
2024-01-20 12:12:50 +0200

    fix: export logic to hook

--- Client/src/components/newComment/NewComment.tsx
+++ Client/src/components/newComment/NewComment.tsx
@@ -4,40 +4,15 @@ import TextField from "@mui/material/TextField/TextField";
 import { useLoggedInUser } from "../../hooks/useLoggedInUser";
 import { commentContainerStyles } from "./styles";
 import { LoadingButton } from "../loadingButton/LoadingButton";
-import { useState } from "react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
-import { backendAxiosInstance } from "../../axios/backendInstance";
 import { Alert } from "@mui/material";
+import { useNewComment } from "./useNewComment";
 
 const CommentContainer = styled("div")(commentContainerStyles);
 
 export const NewComment = () => {
   const { connectedUser, isLoading } = useLoggedInUser();
-  const { postId } = useParams();
-  const queryClient = useQueryClient();
-
-  const postCommentMutation = useMutation({
-    mutationFn: (commentContent: string) =>
-      backendAxiosInstance.post(`/posts/${postId}/comments`, {
-        content: commentContent,
-      }),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["posts", postId] });
-      setContent("");
-    },
-  });
-
-  const [content, setContent] = useState<string>();
-
-  const handleReplyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    postCommentMutation.reset();
-    setContent(e.target.value);
-  };
-
-  const postComment = () => {
-    postCommentMutation.mutate(content);
-  };
+  const { content, handleReplyChange, postComment, isCommentError } =
+    useNewComment();
 
   return (
     !isLoading && (
@@ -60,7 +35,7 @@ export const NewComment = () => {
                 fontSize: "1.2rem",
               },
             }}
-          ></TextField>
+          />
           <LoadingButton
             variant="contained"
             disabled={!content}
@@ -70,9 +45,7 @@ export const NewComment = () => {
             Reply
           </LoadingButton>
         </CommentContainer>
-        {postCommentMutation.isError && (
-          <Alert severity="error">Failed to post reply</Alert>
-        )}
+        {isCommentError && <Alert severity="error">Failed to post reply</Alert>}
       </div>
     )
   );
---
+++ Client/src/components/newComment/useNewComment.tsx
@@ -0,0 +1,38 @@
+import { useParams } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { backendAxiosInstance } from "../../axios/backendInstance";
+import { useState } from "react";
+
+export const useNewComment = () => {
+  const { postId } = useParams();
+  const queryClient = useQueryClient();
+
+  const postCommentMutation = useMutation({
+    mutationFn: (commentContent: string) =>
+      backendAxiosInstance.post(`/posts/${postId}/comments`, {
+        content: commentContent,
+      }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["posts", postId] });
+      setContent("");
+    },
+  });
+
+  const [content, setContent] = useState<string>();
+
+  const handleReplyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    postCommentMutation.reset();
+    setContent(e.target.value);
+  };
+
+  const postComment = () => {
+    postCommentMutation.mutate(content);
+  };
+
+  return {
+    postComment,
+    handleReplyChange,
+    content,
+    isCommentError: postCommentMutation.isError,
+  };
+};
