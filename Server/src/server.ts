60174d6e761707852c43e84e9574341ea8c86701
Amit Keinan <amit.keinan.9@gmail.com>
2024-02-06 15:50:23 +0200

    FEAT: Add final touches for production

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -8,13 +8,13 @@ initApp().then((app) => {
     console.log("development", process.env.PORT);
     http.createServer(app).listen(process.env.PORT);
   } else {
-    // TODO: add pem for production mode
-    // const options = {
-    //   key: fs.readFileSync('../client-key.pem'),
-    //   cert: fs.readFileSync('../client-cert.pem')
-    // };
+    console.log("production");
 
-    // https.createServer(options, app).listen(process.env.HTTPS_PORT);
-    https.createServer(app).listen(process.env.HTTPS_PORT);
+    const options = {
+      key: fs.readFileSync("./client-key.pem"),
+      cert: fs.readFileSync("./client-cert.pem"),
+    };
+
+    https.createServer(options, app).listen(process.env.HTTPS_PORT);
   }
 });
